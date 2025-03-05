a873333adc7c4063b6addcd5cf997103406af5b9
MohsinAliHere <mohsinalihere@gmail.com>
2025-03-05 11:22:40 +0500

    initialize project with Vite, React, and TypeScript setup

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust this path if needed
+	theme: {
+		extend: {
+			colors: {
+				primary: "#4a90e2",
+				secondary: "#50e3c2",
+				background: "#f5f7fa",
+				dark: "#1e2a38",
+				messageSent: "#dcf8c6",
+				messageReceived: "#eaeaea",
+				textPrimary: "#1f2937",
+				textSecondary: "#6b7280",
+				error: "#ff3b30",
+				success: "#34d399",
+			},
+		},
+	},
+	plugins: [],
+};
