40ecf73d90645fd72d63e11c1a7ffb823b33bf12
MohsinAliHere <mohsinalihere@gmail.com>
2025-03-09 04:56:45 +0500

    add authentication check on app load and update CSS variables for new color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import Navigation from "./routes";
+import { useAuthStore } from "./store/authStore";
+import CustomLoader from "./components/CustomLoader";
 
 const App = () => {
+	const { checkAuth, authLoading } = useAuthStore();
+
+	useEffect(() => {
+		checkAuth();
+	}, []);
+
+	if (authLoading) return <CustomLoader />;
+
 	return <Navigation />;
 };
 
