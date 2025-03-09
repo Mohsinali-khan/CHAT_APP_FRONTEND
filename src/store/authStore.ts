ee839e4d14e2b6eaacb3c69b379396d8dcb44a03
MohsinAliHere <mohsinalihere@gmail.com>
2025-03-10 00:19:08 +0500

    add password recovery and reset functionality to auth store

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 import { AUTH_ENDPOINTS } from "../constants/endpoints";
 import { api_instance } from "../config/api";
-import { AuthResponse, User } from "../types/auth";
+import { API_Response, User } from "../types/auth";
 import { AxiosError } from "axios";
+import toast from "react-hot-toast";
 
 interface AuthState {
 	user: User | null;
@@ -15,6 +16,8 @@ interface AuthState {
 	logoutLoading: boolean;
 	authLoading: boolean;
 	verifyOtpLoading: boolean;
+	forgetPasswordloading: boolean;
+	resetPasswordloading: boolean;
 
 	// manage functions
 	login: (
@@ -30,7 +33,15 @@ interface AuthState {
 		email: string,
 		password: string
 	) => Promise<{ success: boolean; message: string }>;
+	forgetPassword: (
+		email: string
+	) => Promise<{ success: boolean; message: string }>;
 	logout: () => Promise<{ success: boolean; message: string }>;
+	resetPassword: (
+		email: string,
+		password: string,
+		otp: string
+	) => Promise<{ success: boolean; message: string }>;
 	checkAuth: () => Promise<void>;
 }
 
@@ -52,13 +63,15 @@ export const useAuthStore = create<AuthState>((set) => {
 		loginLoading: false,
 		signupLoading: false,
 		logoutLoading: false,
+		forgetPasswordloading: false,
+		resetPasswordloading: false,
 		authLoading: true,
 		verifyOtpLoading: false,
 
 		login: async (email, password) => {
 			setLoading("loginLoading", true);
 			try {
-				const { data } = await api_instance.post<AuthResponse>(
+				const { data } = await api_instance.post<API_Response>(
 					AUTH_ENDPOINTS.LOGIN,
 					{ email, password }
 				);
@@ -78,7 +91,7 @@ export const useAuthStore = create<AuthState>((set) => {
 		signup: async (username, email, password) => {
 			setLoading("signupLoading", true);
 			try {
-				const { data } = await api_instance.post<AuthResponse>(
+				const { data } = await api_instance.post<API_Response>(
 					AUTH_ENDPOINTS.SIGNUP,
 					{ username, email, password }
 				);
@@ -98,7 +111,7 @@ export const useAuthStore = create<AuthState>((set) => {
 		verifyOTP: async (otp, email) => {
 			setLoading("verifyOtpLoading", true);
 			try {
-				const { data } = await api_instance.post<AuthResponse>(
+				const { data } = await api_instance.post<API_Response>(
 					AUTH_ENDPOINTS.VERIFY_OTP,
 					{ otp, email }
 				);
@@ -124,13 +137,56 @@ export const useAuthStore = create<AuthState>((set) => {
 		logout: async () => {
 			setLoading("logoutLoading", true);
 			try {
-				await api_instance.get(AUTH_ENDPOINTS.LOGOUT);
+				await api_instance.get<API_Response>(AUTH_ENDPOINTS.LOGOUT);
 				set({ user: null, isAuthenticate: false, message: null });
 				return { success: true, message: "Logged out successfully!" };
 			} catch (error) {
 				return { success: false, message: handleError(error, "Logout failed") };
 			} finally {
 				setLoading("logoutLoading", false);
+			}
+		},
+
+		resetPassword: async (email, password, otp) => {
+			setLoading("resetPasswordloading", true);
+			try {
+				const { data } = await api_instance.post<API_Response>(
+					AUTH_ENDPOINTS.RESET_PASSWORD,
+					{
+						email,
+						password,
+						otp,
+					}
+				);
+				toast.success(data.message);
+				return { success: true, message: "Logged out successfully!" };
+			} catch (error) {
+				const errMsg = handleError(error, "Logout failed");
+				toast.error(errMsg);
+				return { success: false, message: handleError(error, "Logout failed") };
+			} finally {
+				setLoading("resetPasswordloading", false);
+			}
+		},
+
+		forgetPassword: async (email) => {
+			setLoading("forgetPasswordloading", true);
+			try {
+				const { data } = await api_instance.post<API_Response>(
+					AUTH_ENDPOINTS.FORGET_PASSWORD,
+					{ email }
+				);
+				toast.success(data.message);
+				return {
+					success: true,
+					message: data.message || "Password reset email sent!",
+				};
+			} catch (error) {
+				const errMsg = handleError(error, "Password reset failed");
+				toast.error(errMsg);
+				return { success: false, message: errMsg };
+			} finally {
+				setLoading("forgetPasswordloading", false);
 			}
 		},
 
