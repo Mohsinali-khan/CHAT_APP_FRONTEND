14e853f83ca70a7442e9051be554adbf76f0d5d5
MohsinAliHere <mohsinalihere@gmail.com>
2025-03-10 00:19:01 +0500

    refactor auth types for improved readability and consistency

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,25 +1,24 @@
-
 export type User = {
-    username: string;
-    email: string;
-    profilePic: string;
-    otpVerified: boolean;
-    _id: string;
-    __v: number;
+	username: string;
+	email: string;
+	profilePic: string;
+	otpVerified: boolean;
+	_id: string;
+	__v: number;
 };
 
-export type AuthResponse = {
-    success: boolean;
-    message: string;
-    data: {
-        user: User;
-    };
+export type API_Response = {
+	success: boolean;
+	message: string;
+	data: {
+		user: User;
+	};
 };
 
 export type AxiosErrorResponse = {
-    response?: {
-        data: {
-            message:string
-        };
-    };
+	response?: {
+		data: {
+			message: string;
+		};
+	};
 };
